7323a9a8b28df48a36e2d5574655c801a81e0b18
Özkan Çelik <ozkan44celik@gmail.com>
2025-07-30 21:17:57 +0000

    up

--- cost_crafter36/keymaps/vial/config.h
+++ cost_crafter36/keymaps/vial/config.h
@@ -2,7 +2,7 @@
 
 #define VIAL_KEYBOARD_UID {0x59, 0x41, 0x4B, 0x41, 0x4D, 0x4F, 0x5A, 0x31}
 
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 1 } 
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 4
